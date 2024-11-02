63c73132e868fb0ba9f5e65590d8bdc982ddfe97
EDWARD <edward.kang0301@gmail.com>
2024-11-03 08:58:02 +1100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import sqlite3
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
+    QTableWidget
+from datetime import datetime
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        self.table = self.table
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    Main = MainWindow()
+    Main.show()
+    sys.exit(app.exec())
